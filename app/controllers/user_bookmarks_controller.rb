3b953c8e756967943bb22440e288f04d9094f744
Valerie Mettler <valeriemettler@gmail.com>
2014-09-14 11:53:10 -0500

    updated my bookmark view again

--- app/controllers/user_bookmarks_controller.rb
+++ app/controllers/user_bookmarks_controller.rb
@@ -3,13 +3,4 @@ class UserBookmarksController < ApplicationController
     @bookmarks = current_user.bookmarks
     @liked_bookmarks = current_user.likes.collect(&:bookmark)
   end
-
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
 end
