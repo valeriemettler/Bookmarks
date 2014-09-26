0add8ff29909ac0a6c2fa494f2ea7eff7977692c
Valerie Mettler <valeriemettler@gmail.com>
2014-09-26 14:22:49 -0500

    updated embedly

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -19,12 +19,20 @@ class Bookmark < ActiveRecord::Base
  def set_embedly_url
   embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY'],
         :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
+
+  Rails.logger.info ">>>> embedluy object: #{embedly_api.inspect}"
   
   obj = embedly_api.extract :url => self.url
   Rails.logger.info ">>>> embedly obj: #{obj.inspect}"
   # get result and find the attribute for the thumbnail_url
   # set the bookmark's thumbnail_url with that value -- update_attribute(thumbnail_url: val_of_thumbnail_url_from_response)
-  update_attribute(thumbnail_url: obj.images.first['url'])
+  o = obj.first
+  Rails.logger.info ">>>> o: #{o.inspect}"
+  image = o.images.first
+  Rails.logger.info ">>>>> image: #{image.inspect}"
+
+  update_attribute(:thumbnail_url, image['url'])
+  Rails.logger.info ">>>> Now i am: #{self.inspect}"
   true
  end
 
